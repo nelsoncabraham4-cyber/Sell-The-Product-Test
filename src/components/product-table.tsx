461a4338600cfa90dfff7e6b2a7260e12d9c3122
Ansila Safrin <ansilasafrin@gmail.com>
2025-09-14 06:53:24 +0000

    Remove this text (_for element <TableCell>_)

--- src/components/product-table.tsx
+++ src/components/product-table.tsx
@@ -70,7 +70,7 @@ export default function ProductTable({ products, onSale, onDelete, isAdmin }: Pr
             ) : (
               <TableRow>
                 <TableCell colSpan={4} className="h-24 text-center">
-                  No products available. Admins can add products from their dashboard.
+                  
                 </TableCell>
               </TableRow>
             )}
