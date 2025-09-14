7a2081bc370ff4c795864373903db478d3216729
Ansila Safrin <ansilasafrin@gmail.com>
2025-09-14 07:01:07 +0000

    Remove (_for element p_)

--- src/app/admin/dashboard/page.tsx
+++ src/app/admin/dashboard/page.tsx
@@ -63,7 +63,6 @@ export default function AdminDashboardPage() {
     <div className="space-y-8">
       <div>
         <h1 className="font-headline text-4xl font-bold">Admin Dashboard</h1>
-        <p className="text-muted-foreground">Manage products and monitor sales activities.</p>
       </div>
 
       <Tabs defaultValue="dashboard">
